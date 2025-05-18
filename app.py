3a8def2c6fddcc7c7328f3e1786f4269a9c9d3c0
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-05-18 19:06:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ def video_feed(filename):
     return Response(get_frame(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
